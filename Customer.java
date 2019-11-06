f48291909cc0eb1025f255317f6f41b120bd7ab2
thiagommohallem <thiagomohallem1@gmail.com>
2019-11-06 17:52:43 -0300

    COMMIT 5

--- Customer.java
+++ Customer.java
@@ -17,26 +17,6 @@ class Customer {
       return _name;
    };
 
-
-   private double amountFor(Rental aRental) {
-   double result = 0;
-   switch (aRental.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-         result += 2;
-         if (aRental.getDaysRented() > 2)
-            result += (aRental.getDaysRented() - 2) * 1.5;
-         break;
-      case Movie.NEW_RELEASE:
-         result += aRental.getDaysRented() * 3;
-         break;
-      case Movie.CHILDRENS:
-         result += 1.5;
-         if (aRental.getDaysRented() > 3)
-            result += (aRental.getDaysRented() - 3) * 1.5;
-         break;
-   }
-   return result;
-  }
   
   public String statement() {
    double totalAmount = 0;
@@ -47,7 +27,7 @@ class Customer {
       double thisAmount = 0;
       Rental each = (Rental) rentals.nextElement();
 
-      thisAmount = amountFor(each);
+      thisAmount = each.getCharge();
 
       // add frequent renter points
       frequentRenterPoints ++;
--- Rental.java
+++ Rental.java
@@ -12,4 +12,26 @@ class Rental {
     public Movie getMovie() {
       return _movie;
     }
+
+
+   double getCharge() { // veja que não precisa mais de parâmetro
+     double result = 0;
+     switch (getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+           result += 2;
+           if (getDaysRented() > 2)
+              result += (getDaysRented() - 2) * 1.5;
+           break;
+        case Movie.NEW_RELEASE:
+           result += getDaysRented() * 3;
+           break;
+        case Movie.CHILDRENS:
+           result += 1.5;
+           if (getDaysRented() > 3)
+              result += (getDaysRented() - 3) * 1.5;
+           break;
+       }
+      return result;
+    }
+
 }
